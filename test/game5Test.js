b67fceb67cf11720c9608d31fd5f8ca7356c20ba
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-07-08 12:35:16 +0800

    finish puzzle

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -12,10 +13,33 @@ describe('Game5', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+    let address
+    let wallet
+    const provider = await new ethers.getDefaultProvider();
+    console.log(provider)
 
-    await game.win();
+    while (true) {
+      wallet = ethers.Wallet.createRandom(provider);
+      address = wallet.address;
+      console.log(address);
+      if (address < threshold) {
+        break;
+      }
+    }
+
+    const walletTransfer = {
+      to: wallet.address,
+      value: ethers.parseEther('1.0'),
+    };
+    const signer = (await ethers.getSigners())[0];
+    console.log(signer)
+    await signer.sendTransaction(walletTransfer);
+    console.log(await provider.getBalance(wallet.address));
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
   });
 });
+
